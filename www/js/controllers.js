7658f429023c4e80ac8c7b7faad191dc0b713d4b
IbrahimSassi <sassiibrahim12@gmail.com>
2016-07-10 10:35:25 +0100

    'ConsumeTheRemoteApi'

---
+++ www/js/controllers.js
@@ -0,0 +1,35 @@
+var app = angular.module('caffeinehit.controllers', []);
+
+app.controller("YelpController", function ($scope, YelpService) {
+	$scope.yelp = YelpService;
+
+    $scope.doRefresh = function () {
+        if(!$scope.yelp.isLoading)
+        {
+          $scope.yelp.refresh().then(function () {
+              $scope.$broadcast('scroll.refreshComplete');
+          });
+        }
+    };
+
+
+    $scope.loadMore = function () {
+        console.log('loading moreee');
+        if(!$scope.yelp.isLoading && $scope.yelp.hasMore)
+        {
+            $scope.yelp.next().then(function () {
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+            });
+        }
+
+    };
+
+
+    $scope.getDirections = function (cafe) {
+        console.log('Gettin Map');
+    }
+    $scope.openMap = function (cafe) {
+        console.log('openMap');
+    }
+
+});
